b8da38f993c1a7e4c19d6d6af72e21f04a610983
alexytomi <60690056+alexytomi@users.noreply.github.com>
2024-11-26 17:46:09 +0800

    funny wiki

---
+++ docs/.vuepress/.temp/pages/MODS-UNSUPPORTED.html.js
@@ -0,0 +1,16 @@
+import comp from "/data/data/com.termux/files/home/projects/wikirefactor/docs/.vuepress/.temp/pages/MODS-UNSUPPORTED.html.vue"
+const data = JSON.parse("{\"path\":\"/MODS-UNSUPPORTED.html\",\"title\":\"Unsupported Mods and Clients (+ Workarounds/Fixes)\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[{\"level\":2,\"title\":\"Performance Mods + Shaders\",\"slug\":\"performance-mods-shaders\",\"link\":\"#performance-mods-shaders\",\"children\":[]},{\"level\":2,\"title\":\"Skyblock Mods\",\"slug\":\"skyblock-mods\",\"link\":\"#skyblock-mods\",\"children\":[]},{\"level\":2,\"title\":\"Input-related Mods\",\"slug\":\"input-related-mods\",\"link\":\"#input-related-mods\",\"children\":[]},{\"level\":2,\"title\":\"Other Mods\",\"slug\":\"other-mods\",\"link\":\"#other-mods\",\"children\":[]},{\"level\":2,\"title\":\"Unsupported Clients\",\"slug\":\"unsupported-clients\",\"link\":\"#unsupported-clients\",\"children\":[]}],\"git\":{},\"filePathRelative\":\"MODS-UNSUPPORTED.md\"}")
+export { comp, data }
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
